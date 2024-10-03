34c6592b58d4022a4c701e82524fdd6c7b9c7f32
Ahmed Elbelkasy <ahmedelbelkasy25@gmail.com>
2024-10-04 02:11:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Exceptions2
+{
+    internal class Program
+    {
+        public static void Text()
+        {
+            string inpString;
+            Console.WriteLine("enter string");
+            inpString= Console.ReadLine().ToLower();
+            try
+            {
+                if (!inpString.Contains('a') && !inpString.Contains('e') && !inpString.Contains('i') &&
+                    !inpString.Contains('o') && !inpString.Contains('u'))
+                {
+                    throw new Exception("Your string doesn't conatin vowels");
+                }else
+                {
+                    Console.WriteLine($"your string is {inpString}");
+                }
+
+            }
+
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            
+            }
+
+            finally {
+                Console.WriteLine();
+                Console.WriteLine("////////////////////////////////////////////");
+                Console.WriteLine("End of my program");
+            }
+            
+            
+        }
+        static void Main(string[] args)
+        {
+            Text();
+        }
+    }
+}
